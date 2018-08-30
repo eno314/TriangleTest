bba7c8185b52d76bdb551714e7568344e40fccc1
Hiroto Kitamura <kirikonknow@gmail.com>
2018-08-30 16:33:11 +0900

    三角形を作れない場合を用意

--- src/main/kotlin/triangle/Triangle.kt
+++ src/main/kotlin/triangle/Triangle.kt
@@ -2,10 +2,23 @@ package triangle
 
 class Triangle(private val x: Int, private val y: Int, private val z: Int) {
     enum class Type {
-        EQUILATERAL
+        EQUILATERAL,
+        INVALID
     }
 
     fun verify(): Type? {
+        if (x <= 0 || y <= 0 || z <= 0) {
+            return Type.INVALID
+        }
+        if (x + y <= z) {
+            return Type.INVALID
+        }
+        if (y + z <= x) {
+            return Type.INVALID
+        }
+        if (z + x <= y) {
+            return Type.INVALID
+        }
         if (x == y && y == z && z == x) {
             return Type.EQUILATERAL
         }
--- src/test/kotlin/triangle/TriangleTest.kt
+++ src/test/kotlin/triangle/TriangleTest.kt
@@ -9,13 +9,40 @@ object TriangleTest : Spek({
     lateinit var triangle: Triangle
 
     Feature("三角形の種類を判定する") {
+
         Scenario("三角形を作ることができるかの判定") {
+
             Given("3辺のうちどれか1つでも0以下の値を与える") {
                 triangle = Triangle(0, 1, 2)
+            }
+            Then("verifyは無効を返す") {
+                assertEquals(Triangle.Type.INVALID, triangle.verify())
+            }
+
+            Given("x + y が z 以下") {
+                triangle = Triangle(1, 2, 5)
+            }
+            Then("verifyは無効を返す") {
+                assertEquals(Triangle.Type.INVALID, triangle.verify())
+            }
+
+            Given("y + z が x 以下") {
+                triangle = Triangle(3, 2, 1)
+            }
+            Then("verifyは無効を返す") {
+                assertEquals(Triangle.Type.INVALID, triangle.verify())
+            }
+
+            Given("z + x が y 以下") {
+                triangle = Triangle(2, 4, 2)
+            }
+            Then("verifyは無効を返す") {
+                assertEquals(Triangle.Type.INVALID, triangle.verify())
             }
         }
 
         Scenario("正三角形") {
+
             Given("３辺にすべて等しい引数を与える") {
                 triangle = Triangle(1, 1, 1)
             }
