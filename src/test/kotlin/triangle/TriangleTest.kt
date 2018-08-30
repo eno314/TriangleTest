d3876fc81f479285bd01a8a4840ca875903e5114
Hiroto Kitamura <kirikonknow@gmail.com>
2018-08-30 16:14:30 +0900

    kotlinのアサーションライブラリを使う

--- src/test/kotlin/triangle/TriangleTest.kt
+++ src/test/kotlin/triangle/TriangleTest.kt
@@ -1,20 +1,26 @@
 package triangle
 
-import org.assertj.core.api.Assertions
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.gherkin.Feature
+import kotlin.test.assertEquals
 
 object TriangleTest : Spek({
 
     lateinit var triangle: Triangle
 
-    Feature("正三角形の種類を判定する") {
+    Feature("三角形の種類を判定する") {
+        Scenario("三角形を作ることができるかの判定") {
+            Given("3辺のうちどれか1つでも0以下の値を与える") {
+                triangle = Triangle(0, 1, 2)
+            }
+        }
+
         Scenario("正三角形") {
             Given("３辺にすべて等しい引数を与える") {
                 triangle = Triangle(1, 1, 1)
             }
             Then("verifyは正三角形を返す") {
-                Assertions.assertThat(triangle.verify()).isEqualTo(Triangle.Type.EQUILATERAL)
+                assertEquals(Triangle.Type.EQUILATERAL, triangle.verify())
             }
         }
     }
