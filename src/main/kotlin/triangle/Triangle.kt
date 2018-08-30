59e41aa9635767006c731b36a99dbf1f56730766
Hiroto Kitamura <kirikonknow@gmail.com>
2018-08-30 16:56:55 +0900

    事前条件で三角形でないものを弾く

--- src/main/kotlin/triangle/Triangle.kt
+++ src/main/kotlin/triangle/Triangle.kt
@@ -1,24 +1,27 @@
 package triangle
 
 class Triangle(private val x: Int, private val y: Int, private val z: Int) {
+
+    init {
+        if (x <= 0 || y <= 0 || z <= 0) {
+            throw IllegalArgumentException("invalid triangle")
+        }
+        if (x + y <= z) {
+            throw IllegalArgumentException("invalid triangle")
+        }
+        if (y + z <= x) {
+            throw IllegalArgumentException("invalid triangle")
+        }
+        if (z + x <= y) {
+            throw IllegalArgumentException("invalid triangle")
+        }
+    }
+
     enum class Type {
-        EQUILATERAL,
-        INVALID
+        EQUILATERAL
     }
 
     fun verify(): Type? {
-        if (x <= 0 || y <= 0 || z <= 0) {
-            return Type.INVALID
-        }
-        if (x + y <= z) {
-            return Type.INVALID
-        }
-        if (y + z <= x) {
-            return Type.INVALID
-        }
-        if (z + x <= y) {
-            return Type.INVALID
-        }
         if (x == y && y == z && z == x) {
             return Type.EQUILATERAL
         }
--- src/test/kotlin/triangle/TriangleTest.kt
+++ src/test/kotlin/triangle/TriangleTest.kt
@@ -2,50 +2,46 @@ package triangle
 
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.gherkin.Feature
+import org.spekframework.spek2.style.specification.describe
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 
 object TriangleTest : Spek({
 
     lateinit var triangle: Triangle
 
-    Feature("三角形の種類を判定する") {
+    describe("事前条件") {
 
-        Scenario("三角形を作ることができるかの判定") {
+        context("無効な三角形") {
 
-            Given("3辺のうちどれか1つでも0以下の値を与える") {
-                triangle = Triangle(0, 1, 2)
-            }
-            Then("verifyは無効を返す") {
-                assertEquals(Triangle.Type.INVALID, triangle.verify())
-            }
-
-            Given("x + y が z 以下") {
-                triangle = Triangle(1, 2, 5)
-            }
-            Then("verifyは無効を返す") {
-                assertEquals(Triangle.Type.INVALID, triangle.verify())
+            it("3辺のうちどれか1つでも0以下の値を与えたら例外が発生する") {
+                assertFailsWith<IllegalArgumentException> { Triangle(0, 1, 2) }
+                assertFailsWith<IllegalArgumentException> { Triangle(1, -1, 2) }
+                assertFailsWith<IllegalArgumentException> { Triangle(1, 2, -3) }
             }
 
-            Given("y + z が x 以下") {
-                triangle = Triangle(3, 2, 1)
-            }
-            Then("verifyは無効を返す") {
-                assertEquals(Triangle.Type.INVALID, triangle.verify())
+            it("x + y が z 以下の場合に例外が発生する") {
+                assertFailsWith<IllegalArgumentException> { Triangle(1, 2, 5) }
             }
 
-            Given("z + x が y 以下") {
-                triangle = Triangle(2, 4, 2)
+            it("y + z が x 以下の場合に例外が発生する") {
+                assertFailsWith<IllegalArgumentException> { Triangle(3, 2, 1) }
             }
-            Then("verifyは無効を返す") {
-                assertEquals(Triangle.Type.INVALID, triangle.verify())
+
+            it("z + x が y 以下の場合に例外が発生する") {
+                assertFailsWith<IllegalArgumentException> { Triangle(2, 4, 2) }
             }
         }
+    }
+
+    Feature("三角形の種類を判定する") {
 
         Scenario("正三角形") {
 
             Given("３辺にすべて等しい引数を与える") {
                 triangle = Triangle(1, 1, 1)
             }
+
             Then("verifyは正三角形を返す") {
                 assertEquals(Triangle.Type.EQUILATERAL, triangle.verify())
             }
